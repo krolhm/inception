615a48d8eb0e2d69bb7edf8f8d5a993b1a749832
krolhm <raphaelb1@icloud.com>
2021-12-28 15:47:52 +0100

    ⚠

--- srcs/requirements/bonus/ftp/tools/ftp.sh
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
 conf_file="/etc/vsftpd/vsftpd.conf"
-grep -E "local_root=" $conf_file > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  adduser $FTP_USERNAME --disabled-password --gecos "" --home /home/$USER --shell /bin/bash
-  echo "$FTP_USERNAME:$FTP_PASSWORD" | chpasswd > /dev/null
 
-  chgrp -R $FTP_USERNAME $FTP_ROOT
-  chown -R $FTP_USERNAME: $FTP_ROOT
-  chmod -R u+w $FTP_ROOT
+adduser -D $FTP_USERNAME && echo "$FTP_USERNAME:$FTP_PASSWORD" | chpasswd
 
-  echo "" >> $conf_file
-  echo "local_root=$FTP_ROOT" >> $conf_file
-  echo "secure_chroot_dir=$FTP_ROOT" >> $conf_file
-fi
 vsftpd $conf_file
 
