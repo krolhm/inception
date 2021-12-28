d1e22d5200e72301ee7912b825cf681de5ee9d7c
krolhm <raphaelb1@icloud.com>
2021-12-28 12:22:37 +0100

    grafana bonus

--- srcs/requirements/bonus/ftp/tools/ftp.sh
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-adduser -D "${FTP_USERNAME}" && echo "${FTP_USERNAME}":"${FTP_PASSWORD}" | chpasswd
+conf_file="/etc/vsftpd/vsftpd.conf"
+grep -E "local_root=" $conf_file > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  adduser $FTP_USERNAME --disabled-password --gecos "" --home /home/$USER --shell /bin/bash
+  echo "$FTP_USERNAME:$FTP_PASSWORD" | chpasswd > /dev/null
 
-ssh-keygen -A
+  chgrp -R $FTP_USERNAME $FTP_ROOT
+  chown -R $FTP_USERNAME: $FTP_ROOT
+  chmod -R u+w $FTP_ROOT
 
-chown -R "${FTP_USERNAME}":"${FTP_USERNAME}" /home/"${FTP_USERNAME}"
-
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+  echo "" >> $conf_file
+  echo "local_root=$FTP_ROOT" >> $conf_file
+fi
+vsftpd $conf_file
 
