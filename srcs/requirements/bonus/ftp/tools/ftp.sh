7ce03e1696b0b5085ac8706f4c79c6befdca6949
krolhm <raphaelb1@icloud.com>
2021-12-29 20:33:57 +0100

    ⚠

--- srcs/requirements/bonus/ftp/tools/ftp.sh
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -16,7 +16,9 @@ conf_file="/etc/vsftpd/vsftpd.conf"
 
 adduser -D $FTP_USERNAME && echo $FTP_USERNAME:$FTP_PASSWORD} | chpasswd
 
-chown -R $FTP_USERNAME} /home/$FTP_USERNAME}
+mkdir /home/$FTP_USERNAME
+
+chown -R $FTP_USERNAME} /home/$FTP_USERNAME
 
 
 vsftpd $conf_file
